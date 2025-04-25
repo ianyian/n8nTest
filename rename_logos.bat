8d71181da4b469d9b2a2773b4d0a3fea476ea25a
XJ.Yian <ianyian@gmail.com>
2025-04-26 00:03:28 +0800

    update

---
+++ rename_logos.bat
@@ -0,0 +1,12 @@
+@echo off
+cd public\university-logos
+
+echo Renaming university logo files...
+
+ren "monash-university-malaysia.png" "monashuniversitymalaysia.png"
+ren "taylors-university.png" "taylorsuniversity.png"
+ren "universiti-putra-malaysia.png" "universitiputramalaysia.png"
+ren "universiti-teknologi-malaysia.png" "universititeknologimalaysia.png"
+ren "university-of-malaya.png" "universityofmalaya.png"
+
+echo Renaming complete!
